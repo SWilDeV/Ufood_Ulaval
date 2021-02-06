c7dcb2fab53a92aa95871674a4f9842732f4d0c2
SALON-AOUT-2018\escal <mario.rouillard.1@ulaval.ca>
2021-02-06 16:52:52 -0500

    correction de code

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,32 +1,31 @@
 <template>
   <div>
-    <h1 id="restaurantName">Restaurant Normandin Neufchatel</h1>
-    
-    <h3 id="restaurantAdress">2355 Boulevard Bastien, Quebec City, Qc, G2B 1B3</h3>
-    
-    telephone : <a href="tel:+14188426601">(418)842-6601</a>
-    
-    <div><img class="restaurantImage" 
-              src="..\assets\restaurant-normandin.jpg" 
+    <div>
+      <h1 id="restaurantName">Restaurant Normandin Neufchatel</h1>
+      <h3 id="restaurantAdress">2355 Boulevard Bastien, Quebec City, Qc, G2B 1B3</h3>
+      telephone : <a href="tel:+14188426601">(418)842-6601</a>
+    </div>
+
+    <div><img src="@/assets/restaurant-normandin.jpg" 
               alt="Normandin Restaurant" />
     </div>
     
-    <iframe id="restaurantFrame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.452445647169!2d-71.34114555026918!3d46.8567226120164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2s87RCVM46%2BV8!5e0!3m2!1sfr!2sca!4v1612643262694!5m2!1sfr!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.452445647169!2d-71.34114555026918!3d46.8567226120164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2s87RCVM46%2BV8!5e0!3m2!1sfr!2sca!4v1612643262694!5m2!1sfr!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     
-    <button class="directionButton">Directions</button>
+    <button>Directions</button>
     
-    <p id="openingHours">
+    <p>
       Take-out
         Sunday to Saturday : from 9.00am to 7.30pm
       Delivery
         Sunday to Saturday : from 9.00am to 22.00pm 
     </p>
     
-    <div id="typeOfRestaurant">Familial</div>
+    <div>Familial</div>
     
-    <div id="priceAverage">Fourchette de prix : $15 to $30</div>
+    <div>Fourchette de prix : $15 to $30</div>
     
-    <div id="cote">3.5/5</div>    
+    <div>3.5/5</div>    
 
   </div>
 </template>
