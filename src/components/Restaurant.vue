acd34c39c4ce239b7db05a0e6f5435850f7361a7
SALON-AOUT-2018\escal <mario.rouillard.1@ulaval.ca>
2021-02-06 14:41:58 -0500

    Debut page de restaurant

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <h1>Restaurant</h1>
-    <div>GLO-3102 Restaurant page</div>
+    <h1 id="restaurantName">Restaurant Normandin Neufchatel</h1>
+    <h3 id="restaurantAdress">2355 Boulevard Bastien, Quebec City, Qc, G2B 1B3</h3>
+    telephone : <a href="tel:+14188426601">(418)842-6601</a>
+    <div><img class="restaurantImage" 
+              src="..\assets\restaurant-normandin.jpg" 
+              alt="Normandin Restaurant" />
+    </div>
+    <div id="openingHours">
+      Take-out
+        Sunday to Saturday : from 9.00am to 7.30pm
+      Delivery
+        Sunday to Saturday : from 9.00am to 22.00pm 
+    </div>
+    <div id="typeOfRestaurant">Familial</div>
+    <div id="priceAverage">Fourchette de prix : $15 to $30</div>
+    <div id="cote">3.5/5</div>    
+
   </div>
 </template>
 
