0b264af84ca55308ebb11bde069c07254caf32d1
sean wilson <sean0669@gmail.com>
2021-02-08 21:50:59 -0500

    UserPage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,9 @@
       <li>
         <router-link to="/restaurant">Restaurant</router-link>
       </li>
+      <li>
+        <router-link to="/user">UserPage</router-link>
+      </li>
     </ul>
   </div>
 </template>
--- src/components/User.vue
+++ src/components/User.vue
@@ -2,6 +2,54 @@
   <div>
     <h1>User Profile</h1>
     <div>GLO-3102 User Profile page</div>
+    <div class="user-container">
+      <div class="username">
+        <h2>Justin Trudeau</h2>
+      </div>
+      <div class="score">
+        User score: 4.5 / 5
+      </div>
+    </div>
+
+    <div class="restaurant-history">
+
+      <!-- list of visited restaurants -->
+      <ul>
+        <li>
+          <h4>Restaurant Normandin Neufchatel</h4>
+          <div class="restaurant-visited-number">
+            <h6>visited 93432 times</h6>
+          </div>
+        </li>
+
+        <li>
+          <h4>London Jack</h4>
+          <div class="restaurant-visited-number">
+             <h6>visited  3 times</h6>
+          </div>
+        </li>
+
+        <li>
+          <h4>Le Ciel</h4>
+          <div class="restaurant-visited-number">
+             <h6>visited  5 times</h6>
+          </div>
+        </li>
+
+        <li>
+          <h4>MacDonalds</h4>
+          <div class="restaurant-visited-number">
+             <h6>visited  398 times</h6>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+
+    <div class="HomePage">
+      <a href="/">HOME PAGE</a>
+    </div>
+
   </div>
 </template>
 
