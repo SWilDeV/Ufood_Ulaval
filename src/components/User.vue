515abf82cbb4e03a9f57a7be6c98fa0596f8eae3
sean wilson <sean0669@gmail.com>
2021-02-08 22:01:20 -0500

    no class

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,43 +2,43 @@
   <div>
     <h1>User Profile</h1>
     <div>GLO-3102 User Profile page</div>
-    <div class="user-container">
-      <div class="username">
+    <div>
+      <div>
         <h2>Justin Trudeau</h2>
       </div>
-      <div class="score">
+      <div>
         User score: 4.5 / 5
       </div>
     </div>
 
-    <div class="restaurant-history">
+    <div>
 
       <!-- list of visited restaurants -->
       <ul>
         <li>
           <h4>Restaurant Normandin Neufchatel</h4>
-          <div class="restaurant-visited-number">
+          <div>
             <h6>visited 93432 times</h6>
           </div>
         </li>
 
         <li>
           <h4>London Jack</h4>
-          <div class="restaurant-visited-number">
+          <div>
              <h6>visited  3 times</h6>
           </div>
         </li>
 
         <li>
           <h4>Le Ciel</h4>
-          <div class="restaurant-visited-number">
+          <div>
              <h6>visited  5 times</h6>
           </div>
         </li>
 
         <li>
           <h4>MacDonalds</h4>
-          <div class="restaurant-visited-number">
+          <div>
              <h6>visited  398 times</h6>
           </div>
         </li>
@@ -46,7 +46,7 @@
       </ul>
     </div>
 
-    <div class="HomePage">
+    <div>
       <a href="/">HOME PAGE</a>
     </div>
 
