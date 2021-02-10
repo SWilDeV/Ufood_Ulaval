7de36e08eb6c2033ed71cea5ebe86328a74e473c
Francis Pion <francispion@hotmail.com>
2021-02-09 23:19:10 -0500

    Intégrer la déconnexion.

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,5 +2,9 @@ export default {
   login({ commit }, { email, id, name, token }) {
     commit('setToken', token)
     commit('setUser', { email, id, name })
+  },
+  logout({ commit }) {
+    commit('setToken', null)
+    commit('setUser', null)
   }
 }
