7f5212c61a65edcba6903b4dae208de85250697d
SALON-AOUT-2018\escal <mario.rouillard.1@ulaval.ca>
2021-03-14 14:32:20 -0400

    Début configuration google

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -22,10 +22,7 @@
       </div>
       <div class="row">
         <div class="col-sm-12 col-lg-8">
-          <iframe
-            width="100%"
-            height="400px"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.452445647169!2d-71.34114555026918!3d46.8567226120164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2s87RCVM46%2BV8!5e0!3m2!1sfr!2sca!4v1612643262694!5m2!1sfr!2sca"
+          <iframe width="100%" height="400px" src="https://www.google.com/maps/dir// " 
             frameborder="0"
             style="border:0;"
             allowfullscreen=""
