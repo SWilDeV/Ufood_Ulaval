a5d7e21c0cd9d1d7059c19a1db839930c47e9e91
SALON-AOUT-2018\escal <mario.rouillard.1@ulaval.ca>
2021-03-14 20:49:28 -0400

    Travail avec Francis

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="container">
     <template v-if="restaurant">
-      <div class="text-center">
+      <div class="text-center mt-4">
         <div class="imageCell">
           <img class="rounded img-fluid slider" :src="images[currentImage]" />
         </div>
         <h1 class="text-center">{{ restaurant.name }}</h1>
-        <h5 id="restaurantAdress">
+        <h5>
           {{ restaurant.address }}
         </h5>
         <div>Phone: {{ restaurant.tel }}</div>
       </div>
       <br />
       <div class="row">
-        <span class="col-sm-3"
-          >Food types:
-          <span v-for="(type, index) in restaurant.genres" :key="index">{{ type }}, </span></span
-        >
+        <span class="col-sm-3">Food types: {{ restaurant.genres.join(', ') }} </span>
         <span class="col-sm-3">Price range: {{ restaurant.price_range }}/5</span>
         <span class="col-sm-3 font-weight-bold">Rating: {{ restaurantRating }}/5</span>
       </div>
@@ -25,7 +22,7 @@
           <iframe
             width="100%"
             height="400px"
-            :src="googleMapAddress()"
+            src="http://www.google.com/maps/place/@46.8179222,-71.2202176,17z"
             frameborder="0"
             style="border:0;"
             allowfullscreen=""
@@ -37,7 +34,7 @@
         <div class="col-sm-12 col-lg-4">
           <h4>Opening Hours</h4>
           <div>
-            <b-table stripped hover :items="openingHours" :fields="fields" />
+            <b-table striped :items="openingHours" :fields="fields" />
           </div>
         </div>
       </div>
@@ -51,7 +48,6 @@ export default {
   data() {
     return {
       id: '5f31fc6155d7790550c08afe',
-      googleKey: 'AIzaSyBFUveRGozaQvwzxffE5YofybRxNc8PbeU',
       restaurant: null,
       fields: [
         'day',
@@ -61,72 +57,78 @@ export default {
           label: ''
         }
       ],
-
-      images: [],
       currentImage: 0,
       timer: null
     }
   },
 
-  ready: function () {
-    this.startRotation();
-  },
-
   computed: {
+    googleMapAddress() {
+      return (
+        'https://www.google.com/maps/embed/v1/view?key=' +
+        '&center=' +
+        this.restaurant.location.coordinates[0] +
+        ',' +
+        this.restaurant.location.coordinates[1] +
+        '&zoom=14'
+      )
+    },
+
+    images() {
+      return this.restaurant.pictures || []
+    },
+
+    openingHours() {
+      return Object.entries(this.restaurant.opening_hours).map(row => ({
+        day: this.$i18n.t(`weekDays.${row[0]}`),
+        hours: row[1] || 'Closed'
+      }))
+    },
+
+    //   let hoursList = []
+    //   hoursList = this.restaurant.opening_hours
+
+    //   let items = [
+    //     { day: 'Sunday', hours: `${hoursList.sunday}` },
+    //     { day: 'Monday', hours: `${hoursList.monday}` },
+    //     { day: 'Tuesday', hours: `${hoursList.tuesday}` },
+    //     { day: 'Wednesday', hours: `${hoursList.wednesday}` },
+    //     { day: 'Thursday', hours: `${hoursList.thursday}` },
+    //     { day: 'Friday', hours: `${hoursList.friday}` },
+    //     { day: 'Saturday', hours: `${hoursList.saturday}` }
+    //   ]
+
+    //   for (let item in items) {
+    //     if (items[item].hours === 'null') {
+    //       items[item].hours = 'Closed'
+    //     }
+    //   }
+    //   return items
+    // },
+
     restaurantRating() {
-      return Math.round(this.restaurant.rating * 10, 2) / 10
+      return this.restaurant.rating.toFixed(1)
     }
   },
 
   methods: {
-    openingHours() {
-      let hoursList = []
-      hoursList = this.restaurant.opening_hours
-
-      let items = [
-        { day: 'Sunday', hours: `${hoursList.sunday}` },
-        { day: 'Monday', hours: `${hoursList.monday}` },
-        { day: 'Tuesday', hours: `${hoursList.tuesday}` },
-        { day: 'Wednesday', hours: `${hoursList.wednesday}` },
-        { day: 'Thursday', hours: `${hoursList.thursday}` },
-        { day: 'Friday', hours: `${hoursList.friday}` },
-        { day: 'Saturday', hours: `${hoursList.saturday}` }
-      ]
-
-      for (let item in items) {
-        if (items[item].hours === 'null') {
-          items[item].hours = 'Closed'
-        }
-      }
-      return items
+    startRotation() {
+      this.timer = setInterval(this.next, 3000)
     },
 
-    startRotation: function() {
-      this.timer = setInterval(this.next, 3000);
-    },
-
-    next: function() {
-      this.currentImage === this.images.length - 1
-        ? (this.currentImage = 0)
-        : (this.currentImage += 1)
-    },
-
-    googleMapAddress: function() {
-      return (
-        'https://www.google.com/maps/embed/v1/view?key=' +
-        this.googleKey + '&center=' +
-        this.restaurant.location.coordinates[0] +
-        ',' +
-        this.restaurant.location.coordinates[1] +
-        '&zoom=14')
+    next() {
+      this.currentImage = this.currentImage === this.images.length - 1 ? 0 : this.currentImage + 1
     }
   },
 
   async created() {
-    const response = await get(`/unsecure/restaurants/${this.$route.params.id}`)
-    this.restaurant = await response.json()
-    this.images = this.restaurant.pictures
-    this.startRotation()
+    try {
+      const response = await get(`/unsecure/restaurants/${this.$route.params.id}`)
+      this.restaurant = await response.json()
+      this.startRotation()
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 </script>
