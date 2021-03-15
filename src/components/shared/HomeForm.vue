2812c6452f2ed06c0fb9d2ffeddbcc13eecc84d3
Alec Vincent <alec.vincent.1@ulaval.ca>
2021-03-15 06:50:42 -0400

    Version avec le home-form fonctionel

---
+++ src/components/shared/HomeForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-form">
+    <homeHeader></homeHeader>
+    <restaurantsList></restaurantsList>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './HomeHeader'
+import RestaurantsList from './RestaurantsList'
+
+export default {
+  name: 'homeForm',
+  components: { HomeHeader, RestaurantsList }
+}
+</script>
+
+<style></style>
