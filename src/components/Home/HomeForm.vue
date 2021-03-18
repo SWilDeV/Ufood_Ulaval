2ec5c0fd0530c9cf65b216cb417742215a8dde63
Francis Pion <francispion@hotmail.com>
2021-03-17 20:38:32 -0400

    Accueil pagination

--- src/components/Home/HomeForm.vue
+++ src/components/Home/HomeForm.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="home-form">
+  <div>
     <homeHeader
       v-bind:allGenres="allGenres"
       v-bind:allPriceRanges="allPriceRanges"
-      v-bind:refresh="refresh"
-    ></homeHeader>
-    <restaurantsList v-bind:allRestaurants="allRestaurants"></restaurantsList>
+      @filters-changed="updateFilters($event)"
+    />
+    <restaurantsList v-bind:allRestaurants="allRestaurants" />
+    <div class="form-inline">
+      <button class="btn btn-primary m-1" :disabled="page === 1" @click="page = 1">
+        <font-awesome-icon icon="fast-backward" />
+      </button>
+      <button class="btn btn-primary m-1" :disabled="page === 1" @click="page--">
+        <font-awesome-icon icon="step-backward" />
+      </button>
+      <input type="text" class="form-control m-1" v-model="page" disabled />
+      <button class="btn btn-primary m-1" :disabled="page === pageCount" @click="page++">
+        <font-awesome-icon icon="step-forward" />
+      </button>
+      <button class="btn btn-primary m-1" :disabled="page === pageCount" @click="page = pageCount">
+        <font-awesome-icon icon="fast-forward" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -22,44 +37,64 @@ export default {
     return {
       allRestaurants: [],
       allGenres: [],
-      allPriceRanges: []
+      allPriceRanges: [],
+      count: 12,
+      genre: '',
+      page: 1,
+      price: '',
+      search: '',
+      total: 0
+    }
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.total / this.count)
+    },
+    params() {
+      return {
+        genre: this.genre,
+        page: this.page,
+        price: this.price,
+        search: this.search
+      }
     }
   },
   async created() {
     try {
-      const restaurantsResponse = await get('/unsecure/restaurants?limit=130')
-      this.allRestaurants = restaurantsResponse.items
+      const results = await get('/unsecure/restaurants?limit=1000')
+
+      //On va chercher la liste de tous les genres différents:
+      const restaurantsNumber = results.items.length
+      for (let i = 0; i < restaurantsNumber; i++) {
+        const genresForRestaurantArray = results.items[i].genres
+        const genresForRestaurantNumber = genresForRestaurantArray.length
+        for (let j = 0; j < genresForRestaurantNumber; j++) {
+          if (!this.allGenres.includes(genresForRestaurantArray[j])) {
+            this.allGenres.push(genresForRestaurantArray[j])
+          }
+        }
+      }
+
+      //On va chercher toutes les fourchettes de prix différentes:
+      for (let i = 0; i < restaurantsNumber; i++) {
+        const priceRange = results.items[i].price_range
+        if (!this.allPriceRanges.includes(priceRange) && typeof priceRange !== 'undefined') {
+          this.allPriceRanges.push(priceRange)
+        }
+      }
+      this.allPriceRanges.sort((a, b) => a - b)
     } catch (e) {
       console.error(e)
     }
-
-    //On va chercher la liste de tous les genres différents:
-    let restaurantsNumber = this.allRestaurants.length
-    for (let i = 0; i < restaurantsNumber; i++) {
-      let genresForRestaurantArray = this.allRestaurants[i].genres
-      let genresForRestaurantNumber = genresForRestaurantArray.length
-      for (let j = 0; j < genresForRestaurantNumber; j++) {
-        if (!this.allGenres.includes(genresForRestaurantArray[j])) {
-          this.allGenres.push(genresForRestaurantArray[j])
-        }
-      }
-    }
-
-    //On va chercher toutes les fourchettes de prix différentes:
-    for (let i = 0; i < restaurantsNumber; i++) {
-      let priceRange = this.allRestaurants[i].price_range
-      if (!this.allPriceRanges.includes(priceRange) && typeof priceRange !== 'undefined') {
-        this.allPriceRanges.push(priceRange)
-      }
-    }
-    this.allPriceRanges.sort(function(a, b) {
-      return a - b
-    })
   },
-
   methods: {
-    async refresh({ genre, price, search }) {
-      const query = ['limit=130']
+    updateFilters({ genre, price, search }) {
+      this.genre = genre
+      this.price = price
+      this.search = search
+    },
+    async refresh({ genre, page, price, search }) {
+      const query = [`limit=${this.count}`, `page=${page - 1}`]
       if (genre) {
         query.push(`genres=${genre}`)
       }
@@ -72,10 +107,20 @@ export default {
 
       const queryString = query.join('&')
       try {
-        const restaurantsResponse = await get(`/unsecure/restaurants?${queryString}`)
-        this.allRestaurants = restaurantsResponse.items
+        const results = await get(`/unsecure/restaurants?${queryString}`)
+        this.allRestaurants = results.items
+        this.total = results.total
       } catch (e) {
         console.error(e)
+      }
+    }
+  },
+  watch: {
+    params: {
+      deep: true,
+      immediate: true,
+      handler(value) {
+        this.refresh(value)
       }
     }
   }
