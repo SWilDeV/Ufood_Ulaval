a72063c401b5ad086a8cee957a8056941c1a84b8
SALON-AOUT-2018\escal <mario.rouillard.1@ulaval.ca>
2021-04-05 21:23:56 -0400

    Page restaurant i18n complete

---
+++ src/components/shared/RestaurantCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="card cell-restaurant col-sm-12 col-md-6 col-lg-4">
+    <img v-bind:src="restaurantPicture" class="rounded mx-auto d-block" height="250" />
+    <div class="card-body">
+      <h4 class="card-title">{{ restaurant.name }}</h4>
+      <p class="card-text">
+        Adress: {{ restaurant.address }}<br />
+        Price range: {{ restaurant.price_range }}<br />
+        Food genres: {{ restaurantGenres }}<br />
+        Rating: {{ restaurant.rating.toFixed(2) }}
+      </p>
+      <orderButton class="button" v-bind:restaurant-id="restaurant.id"></orderButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import OrderButton from './OrderButton'
+
+export default {
+  name: 'restaurantCard',
+  components: { OrderButton },
+  props: ['restaurant'],
+
+  computed: {
+    restaurantPicture: function() {
+      return this.restaurant.pictures[0]
+    },
+
+    restaurantGenres: function() {
+      return this.restaurant.genres.join(' | ')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cell-restaurant {
+  padding-top: 2%;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2em;
+  box-shadow: 5px 10px #888888;
+}
+
+p {
+  color: #333333;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  font-size: 20px;
+  font-weight: 300;
+  line-height: 1.5625;
+  margin-bottom: 15px;
+}
+
+h4 {
+  color: #111;
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 32px;
+  margin: 0 0 24px;
+  padding: 0 30px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.button {
+  position: absolute;
+  bottom: 5px;
+}
+</style>
