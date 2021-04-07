c65dcad05ea4c3fb7e33077b03755f9185c1ad46
Francis Pion <francispion@hotmail.com>
2021-04-07 12:01:18 -0400

    Added cookie expiration.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,16 @@
 import Cookies from 'js-cookie'
 
+const getExpiration = token => {
+  const parts = token.split('.')
+  const decoded = atob(parts[1])
+  const claims = JSON.parse(decoded)
+  return new Date(claims.exp)
+}
+
 export default {
   login({ commit }, { email, id, name, token }) {
-    Cookies.set('token', token)
+    const expires = getExpiration(token)
+    Cookies.set('token', token, { expires })
     commit('setUser', { email, id, name })
   },
   logout({ commit }) {
