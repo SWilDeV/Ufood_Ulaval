6bb2bb7d925f5c96dbc00b7f7acddadc23418995
Francis Pion <francispion@hotmail.com>
2021-04-17 21:31:38 -0400

    Ajout message d'erreur

--- src/components/Navigation/ErrorAlert.vue
+++ src/components/Navigation/ErrorAlert.vue
@@ -1,12 +1,12 @@
 <template>
   <b-alert
-    :show="errorCounter"
     dismissible
     variant="danger"
+    :show="errorCounter"
     @dismissed="hideError"
     @dismiss-count-down="updateError"
   >
-    <p>This alert will dismiss after {{ errorCounter }} seconds...</p>
+    <p v-t="'genericError'" />
     <b-progress variant="danger" :max="10" :value="errorCounter" height="4px" />
   </b-alert>
 </template>
